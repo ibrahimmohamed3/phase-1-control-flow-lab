c486975b3d35bc614b9d3fe7f3a5224adc5988bb
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-06-14 17:47:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return "Enjoy your free sample!";
+  } else {
+    return "This is not a free ride.";
+  }
+  }
+
 
 function ternaryCheckCity(){
   // Write your code here!
